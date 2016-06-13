1f7fc2e14f3598d3a3e58d15314e1e5a653f7793
Tobias Johansson <tobiajo@kth.se>
2016-06-13 03:50:55 +0200

    Add a test

--- cmdlinetool/test/test_util.py
+++ cmdlinetool/test/test_util.py
@@ -3,6 +3,8 @@ from cmdlinetool import util
 
 
 class TestUtil(unittest.TestCase):
-
     def test_adder(self):
         self.assertEqual(util.adder(3, 5), 8)
+
+    def test_adder2(self):
+        self.assertEqual(util.adder(-3, -5), -8)
